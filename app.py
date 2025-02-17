32d85babc9cd67f2c05b1965364afb3ed2305509
eomaximoz <gustavo.maximo@escola.pr.gov.br>
2025-02-17 10:15:37 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+print("""
+
+╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
+┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯
+┃╰━━┳━━┫╰━┳━━┳━╮┃╰━━┳╮╭┳━━┳━┳━━┳━━┳━━╮
+╰━━╮┃╭╮┃╭╮┃╭╮┃╭╯┃╭━━┻╋╋┫╭╮┃╭┫┃━┫━━┫━━┫
+┃╰━╯┃╭╮┃╰╯┃╰╯┃┃╱┃╰━━┳╋╋┫╰╯┃┃┃┃━╋━━┣━━┃
+╰━━━┻╯╰┻━━┻━━┻╯╱╰━━━┻╯╰┫╭━┻╯╰━━┻━━┻━━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
+      """)
+
+print("1 - Cadastrar restaurante")
+print("2 - Listar restaurante ")
+print("3 - Ativar restaurante")
+print("4 - Sair")
+
+opcao_escolhida = int (input('Escolha uma opção: '))
+print(f"Você escolheu a opção {opcao_escolhida}")
+
+if opcao_escolhida ==1:
+    print('cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('listar restaurante')
+elif opcao_escolhida ==3
+    print('ativar restaurante')
+else:
+    print('encerrando programa')
+ 
